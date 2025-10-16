883aad6cdf7a5cf96642fafbc0341e75aead8020
2ELCN0168 <lcn.en.perso+github@protonmail.com>
2025-10-16 11:18:45 +0200

    Updated waybar configuration

---
+++ waybar/waybar-crypto.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source "${HOME}/.cache/wal/colors.sh"
+
+content="$(waybar-crypto)"
+
+filter="$(echo "${content}" | jq -r '.text')"
+
+if [[ "${filter}" == *-* ]]; then
+    echo "<span foreground=\"${color1}\">${filter}</span>"
+elif [[ "${filter}" == *+* ]]; then
+    echo "<span foreground=\"${color2}\">${filter}</span>"
+fi
